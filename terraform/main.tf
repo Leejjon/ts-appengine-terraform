38b317b05a7719e13bc6c305391de496f2c19495
Leejjon <leejjon@gmail.com>
2023-08-12 23:45:02 +0200

    My terraform setup.

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,7 +27,85 @@ resource "google_storage_bucket" "terraform_state" {
   }
 }
 
-#resource "google_app_engine_application" "ts-appengine-app" {
-#  project     = "node-on-gcp-terraform"
-#  location_id = "us-central"
-#}
+resource "google_app_engine_application" "ts-appengine-app" {
+  project     = var.project_name
+  location_id = "us-central"
+#  database_type = "CLOUD_DATASTORE_COMPATIBILITY"
+}
+
+resource "google_storage_bucket" "app" {
+  name          = "${var.project_name}-${random_id.app.hex}"
+  location      = "US"
+  force_destroy = true
+  versioning {
+    enabled = true
+  }
+}
+
+resource "random_id" "app" {
+  byte_length = 8
+}
+
+resource "google_storage_bucket_object" "app" {
+  name   = "app.zip"
+  source = "../app/app.zip"
+  bucket = google_storage_bucket.app.name
+  depends_on = [
+    null_resource.build_and_push
+  ]
+}
+
+resource "null_resource" "build_and_push" {
+  provisioner "local-exec" {
+    command = "zip ../app/app.zip ../app/index.js ../app/package.json ../app/package-lock.json"
+  }
+
+  triggers = {
+    always_run = timestamp()
+  }
+
+  depends_on = [
+    google_storage_bucket.app
+  ]
+}
+
+resource "google_app_engine_application_url_dispatch_rules" "ts-appengine-app-dispatch-rules" {
+  dispatch_rules {
+    domain = "*"
+    path = "/*"
+    service = "default"
+  }
+}
+
+resource "google_app_engine_standard_app_version" "app_v2" {
+  version_id = "2"
+  service    = "default"
+  runtime    = "nodejs18"
+
+  entrypoint {
+    shell = "node index.js"
+  }
+
+  deployment {
+
+    zip {
+      source_url = "https://storage.googleapis.com/${google_storage_bucket.app.name}/${google_storage_bucket_object.app.name}"
+    }
+  }
+
+  automatic_scaling {
+    max_concurrent_requests = 10
+    min_idle_instances      = 1
+    max_idle_instances      = 3
+    min_pending_latency     = "1s"
+    max_pending_latency     = "5s"
+    standard_scheduler_settings {
+      target_cpu_utilization        = 0.5
+      target_throughput_utilization = 0.75
+      min_instances                 = 0
+      max_instances                 = 4
+    }
+  }
+  delete_service_on_destroy = true
+}
+
