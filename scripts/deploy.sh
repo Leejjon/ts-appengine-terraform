07ee04234651e8854a876337f9d86cea968d9b43
Leejjon <leejjon@gmail.com>
2023-09-02 01:35:59 +0200

    Added an extra comment.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,4 +7,6 @@ cd ../terraform || exit
 terraform apply -auto-approve -var="deployment_version=${dateString}"
 
 cd ../scripts || exit
+
+# Point 100% of the traffic to the new version
 gcloud app services set-traffic default --splits=$dateString=1 --quiet
