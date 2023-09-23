5c409ffe05b37e5ed0d0b39d8b4a57aebce558f1
Leejjon <leejjon@gmail.com>
2023-09-24 00:38:43 +0200

    I guess I fixed my bug already.

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,12 +1,31 @@
 'use strict';
 
 import express, {Request, Response} from "express";
+import {Datastore} from "@google-cloud/datastore";
 
 const app = express();
+const router = express.Router();
 
-app.use((req: Request, res: Response) => {
-    res.status(200).send('Hello, world!');
+router.post("/create", async (req: Request, res: Response) => {
+    res.contentType('application/json');
+    try {
+        const datastore = new Datastore();
+
+        res.status(200);
+        res.send({});
+    } catch (error) {
+        res.status(200);
+        res.send({message: error.toString()});
+    }
 });
+
+router.get("/query", async (req: Request, res: Response) => {
+    res.contentType('application/json');
+    res.status(200);
+    res.send({});
+});
+
+app.use(router);
 
 // Start the server
 const PORT = process.env.PORT || 8080;
