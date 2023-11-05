741b72782e59c0ce91485b2e30b9d9b65b55d935
Leejjon <leejjon@gmail.com>
2023-11-05 19:10:06 +0100

    Removed uneccesary flag.

--- app/dist/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const app = express();
-const port = process.env.PORT || 8080;
-app.get('/', (req, res) => {
-    res.send('Hello world10!');
-});
-app.listen(port, function () {
-    console.log(`App is listening on port ${port} !`);
-});
